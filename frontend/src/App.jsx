3cb450ee21f311326a5b0c02e89bf87135a9b9bc
Bikash Khanal <khanalbikash007@gmail.com>
2026-02-22 11:45:37 +0545

    tailwindcss setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-   <div>Codex | Smart Education system</div>
+   <div className='text-center text-4xl text-blue-600'>Codex | Smart Education system</div>
   )
 }
 
